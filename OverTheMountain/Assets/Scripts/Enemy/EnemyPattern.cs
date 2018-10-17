20b71c426b90e3513c5356fa915db74714d61a1b
Arthur Uzulin (fuj1n) <fuj1nman@hotmail.com>
2018-10-17 15:59:41 +1100

    Initial Commit

---
+++ OverTheMountain/Assets/Scripts/Enemy/EnemyPattern.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Pattern", menuName = "Enemy Pattern")]
+public class EnemyPattern : ScriptableObject {
+    
+}
---
+++ OverTheMountain/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    public float speed = 10F;
+
+    private Rigidbody2D r2d;
+
+    private void Awake()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        float up = Input.GetAxis("Vertical");
+        float right = Input.GetAxis("Horizontal");
+
+        r2d.velocity = new Vector2(right * speed, up * speed);
+    }
+}
