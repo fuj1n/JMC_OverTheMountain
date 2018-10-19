28026aaf29c66a68ce64ef64a7b7cae1f9a4f838
Arthur Uzulin <fuj1nman@hotmail.com>
2018-10-19 13:34:54 +1100

    Basic scrolling background, no generation yet

--- OverTheMountain/Assets/Scripts/Background/ScrollingBackground.cs
+++ OverTheMountain/Assets/Scripts/Background/ScrollingBackground.cs
@@ -12,7 +12,49 @@ public class ScrollingBackground : MonoBehaviour
     private static readonly Dictionary<string, Sprite[]> spawnables = new Dictionary<string, Sprite[]>();
     private static readonly Dictionary<string, float> rotations = new Dictionary<string, float>();
 
-    [RuntimeInitializeOnLoadMethod]
+    public Vector2Int tilesCount = new Vector2Int(4, 6);
+    public Vector2 tilesGap = new Vector2(10, 10);
+
+    public float scrollSpeed = 5F;
+    public float resetsAt = 20F;
+
+    private Transform[,] tiles;
+
+    private Transform anchor;
+
+    private float scrollValue = 0F;
+
+    private void Awake()
+    {
+        anchor = new GameObject("Anchor").transform;
+        anchor.SetParent(transform, false);
+
+        tiles = new Transform[tilesCount.x, tilesCount.y];
+
+        for (int x = 0; x < tiles.GetLength(0); x++)
+        {
+            for (int y = 0; y < tiles.GetLength(1); y++)
+            {
+                GameObject tile = new GameObject("Tile at X:" + x + " number " + y);
+                tile.transform.SetParent(anchor);
+                tile.AddComponent<SpriteAnimator>().Sprites = sprites.First().Value;
+
+                tile.transform.localPosition = new Vector2(tilesGap.x * x, tilesGap.y * y);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        scrollValue += scrollSpeed * Time.deltaTime;
+
+        if (scrollValue >= resetsAt)
+            scrollValue = 0;
+
+        anchor.SetAxisPosition(Utility.Axis.Y, -scrollValue, true);
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initialize()
     {
         BasicTile[] tiles = Resources.LoadAll<BasicTile>(TILES_PATH);
@@ -30,17 +72,8 @@ public class ScrollingBackground : MonoBehaviour
             int spriteWidth = (int)tile.sprite.rect.width;
             int spriteHeight = (int)tile.sprite.rect.height;
 
-            if (spriteWidth < spriteHeight)
-            {
-                Debug.LogWarning("Sprite " + tile.name + " is higher than it is wide, skipping...");
+            if (!ValidateSprite(tile.sprite))
                 continue;
-            }
-
-            if (spriteWidth % spriteHeight != 0)
-            {
-                Debug.LogWarning("Sprite " + tile.name + " height cannot be evenly divided into width, skipping...");
-                continue;
-            }
 
             sprites[tile.name] = ChopSprite(tile.sprite);
             frameRates[tile.name] = tile.fps;
@@ -50,28 +83,36 @@ public class ScrollingBackground : MonoBehaviour
                 ConnectedTile ct = (ConnectedTile)tile;
 
                 Sprite[] u, d, l, r;
-                u = ChopSprite(ct.up);
-
-                if (ct.useRotation)
+                if (ValidateSprite(ct.up))
                 {
-                    d = l = r = u;
+                    u = ChopSprite(ct.up);
 
-                    rotations[tile.name + "$u"] = ct.rotationUp;
-                    rotations[tile.name + "$d"] = ct.rotationDown;
-                    rotations[tile.name + "$l"] = ct.rotationLeft;
-                    rotations[tile.name + "$r"] = ct.rotationRight;
-                }
-                else
-                {
-                    d = ChopSprite(ct.down);
-                    l = ChopSprite(ct.left);
-                    r = ChopSprite(ct.right);
-                }
+                    if (ct.useRotation)
+                    {
+                        d = l = r = u;
 
-                sprites[tile.name + "$u"] = u;
-                sprites[tile.name + "$d"] = d;
-                sprites[tile.name + "$l"] = l;
-                sprites[tile.name + "$r"] = r;
+                        rotations[tile.name + "$u"] = ct.rotationUp;
+                        rotations[tile.name + "$d"] = ct.rotationDown;
+                        rotations[tile.name + "$l"] = ct.rotationLeft;
+                        rotations[tile.name + "$r"] = ct.rotationRight;
+                    }
+                    else
+                    {
+                        if (ValidateSprite(ct.down, "using UP...")) d = ChopSprite(ct.down);
+                        else d = u;
+
+                        if (ValidateSprite(ct.left, "using UP...")) l = ChopSprite(ct.left);
+                        else l = u;
+
+                        if (ValidateSprite(ct.right, "using UP...")) r = ChopSprite(ct.right);
+                        else r = u;
+                    }
+
+                    sprites[tile.name + "$u"] = u;
+                    sprites[tile.name + "$d"] = d;
+                    sprites[tile.name + "$l"] = l;
+                    sprites[tile.name + "$r"] = r;
+                }
             }
 
             if (tile.modifiers.spawnables != null)
@@ -97,6 +138,7 @@ public class ScrollingBackground : MonoBehaviour
                             for (int y = 0; y < spriteHeight / spawnable.cellSize; y++)
                             {
                                 sb.Add(Sprite.Create(spawnable.sprite.texture, new Rect(x * cellSize, y * cellSize, cellSize, cellSize), Vector2.one / 0.5F, spawnable.sprite.pixelsPerUnit));
+                                sb.Last().name = spawnable.sprite.name + " cutout " + x * spriteWidth + y;
                             }
                         }
                     }
@@ -109,15 +151,6 @@ public class ScrollingBackground : MonoBehaviour
                 spawnables[tile.name] = sb.ToArray();
             }
         }
-
-        //for (int i = 0; i < sprites.First().Value.Length; i++)
-        //{
-        //    GameObject g = new GameObject("Test" + i);
-        //    g.AddComponent<SpriteRenderer>().sprite = sprites.First().Value[i];
-        //    g.transform.Translate(Vector3.right * i * 10 + Vector3.forward * 10);
-        //}
-
-        new GameObject("Test").AddComponent<SpriteAnimator>().Sprites = sprites.First().Value;
     }
 
     private static Sprite[] ChopSprite(Sprite s)
@@ -129,8 +162,31 @@ public class ScrollingBackground : MonoBehaviour
 
         // Chops up the sprite into smaller sprites for animation
         for (int i = 0; i < sprites.Length; i++)
-            sprites[i] = Sprite.Create(s.texture, new Rect(spriteHeight * i, 0, spriteHeight, spriteHeight), Vector3.one * 0.5F, s.pixelsPerUnit);
+        {
+            sprites[i] = Sprite.Create(s.texture, new Rect(spriteHeight * i, 0, spriteHeight, spriteHeight), Vector2.zero, s.pixelsPerUnit);
+            sprites[i].name = s.name + " frame " + i;
+        }
 
         return sprites;
+    }
+
+    private static bool ValidateSprite(Sprite s, string errorSuffix = "skipping...")
+    {
+        int spriteWidth = (int)s.rect.width;
+        int spriteHeight = (int)s.rect.height;
+
+        if (spriteWidth < spriteHeight)
+        {
+            Debug.LogWarning("Sprite " + s.name + " is higher than it is wide, " + errorSuffix);
+            return false;
+        }
+
+        if (spriteWidth % spriteHeight != 0)
+        {
+            Debug.LogWarning("Sprite " + s.name + ": height cannot be evenly divided into width, " + errorSuffix);
+            return false;
+        }
+
+        return true;
     }
 }
---
+++ OverTheMountain/Assets/Scripts/Utils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+public static class Utility
+{
+    /// <summary>
+    /// Sets the position on specified <paramref name="axis"/> of the <paramref name="t"/>
+    /// </summary>
+    /// <param name="t">The transform</param>
+    /// <param name="axis">The axis to set</param>
+    /// <param name="value">The value to set the axis to</param>
+    /// <param name="local">Whether to do this to local position</param>
+    public static void SetAxisPosition(this Transform t, Axis axis, float value, bool local = false)
+    {
+        Vector3 pos = local ? t.localPosition : t.position;
+        if (axis.HasFlag(Axis.X))
+            pos.x = value;
+        if (axis.HasFlag(Axis.Y))
+            pos.y = value;
+        if (axis.HasFlag(Axis.Z))
+            pos.z = value;
+
+        if (local)
+            t.localPosition = pos;
+        else
+            t.position = pos;
+    }
+
+    /// <summary>
+    /// Isolates the given <paramref name="axis"/>
+    /// </summary>
+    /// <param name="source">Source Vector</param>
+    /// <param name="axis">The axis to keep</param>
+    /// <returns><paramref name="source"/>, but where the unspecified axis are removed</returns>
+    public static Vector3 Isolate(this Vector3 source, Axis axis)
+    {
+        if (!axis.HasFlag(Axis.X))
+            source.x = 0;
+        if (!axis.HasFlag(Axis.Y))
+            source.y = 0;
+        if (!axis.HasFlag(Axis.Z))
+            source.z = 0;
+
+        return source;
+    }
+
+    /// <summary>
+    /// Calls <see cref="Isolate(Vector3, Axis)"/> with an inverted <paramref name="axis"/>
+    /// </summary>
+    /// <param name="source">Source Vector</param>
+    /// <param name="axis">The axis to remove</param>
+    /// <returns><paramref name="source"/>, but where the specified axis are removed</returns>
+    public static Vector3 Remove(this Vector3 source, Axis axis)
+    {
+        return Isolate(source, ~axis);
+    }
+
+    [Flags]
+    public enum Axis
+    {
+        NONE = 0,
+        X = 1,
+        Y = 2,
+        Z = 4
+    }
+}
